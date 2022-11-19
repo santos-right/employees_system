4b81b28028edb413a22dfc1dda6d6204348aa3df
philip santos <santos.philip385@gmail.com>
2022-11-19 12:41:20 +0100

    The complete project

---
+++ app/Filament/Resources/EmployeeResource/Widgets/EmployeeStatsOverview.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Filament\Resources\EmployeeResource\Widgets;
+
+use App\Models\Country;
+use App\Models\Employee;
+use Filament\Widgets\StatsOverviewWidget\Card;
+use Filament\Widgets\StatsOverviewWidget as BaseWidget;
+
+class EmployeeStatsOverview extends BaseWidget
+{
+    protected function getCards(): array
+    {
+        $us = Country::where('country_code', 'US')->withCount('employees')->first();
+        $uk = Country::where('country_code', 'UK')->withCount('employees')->first();
+        return [
+            Card::make('All Employees', Employee::all()->count()),
+            Card::make('Uk Employees', $uk ? $uk->employees_count : 0),
+            Card::make('US Employees', $us ? $us->employees_count : 0),
+        ];
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,9 @@
 <?php
 
+use App\Models\Employee;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Resources\EmployeeResource;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,6 +16,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
+
+Route::get('/employees', function() {
+    $employees = Employee::orderBy('last_name')->get();
+
+    return EmployeeResource::collection($employees);
 });
